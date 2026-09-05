cf57f10d8c1976de6ce78abb7bb9b0a428b272f2
keaido666 <730583240@qq.com>
2026-09-05 14:07:05 +0800

    Update nav.js

--- common/nav.js
+++ common/nav.js
@@ -6,4 +6,16 @@ function initNav(){
             floatMenu.classList.toggle('collapsed');
         });
     }
+
+    // 新增：设置导航active高亮
+    const links = document.querySelectorAll('.menu-link');
+    const currentHref = window.location.href;
+    links.forEach(link=>{
+        // 去掉原有active
+        link.classList.remove('active');
+        // 判断当前页面url是否匹配导航a标签的href
+        if(currentHref.includes(link.href)){
+            link.classList.add('active');
+        }
+    })
 }
